7c1023ec9a0f68026c14de3cf3453158fb1d60ed
System Administrator <root@Razors-MacBook-Air.local>
2019-03-29 21:41:02 +0530

    Welcome Component Created

--- App.js
+++ App.js
@@ -1,21 +1,133 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ Component } from 'react';
+import  * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { Container, Button,Header,Left,Body,Right,Title ,Text,StyleProvider} from 'native-base';
+StyleSheet.setStyleAttributePreprocessor('fontFamily', Font.processFontFamily);
+import { StyleSheet, View,Image } from 'react-native';
+import Carousel from './Imageslider.js';
+import {StatusBar} from 'react-native';
+
+
 
 export default class App extends React.Component {
+
+   async componentDidMount() {
+     await Font.loadAsync({
+      'Titi':require('./assets/fonts/TitilliumWeb-Regular.ttf'),
+      'Roboto': require('./node_modules/native-base/Fonts/Roboto.ttf'),
+       'Roboto_medium': require('./node_modules/native-base/Fonts/Roboto_medium.ttf'),
+      
+      ...Ionicons.font,
+    });
+
+    
+  }
+  
   render() {
+    
     return (
+
+      
+
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+
+{console.log(StatusBar.currentHeight)}
+      <View style={styles.slider}>
+
+      <Carousel pageStyle={{backgroundColor : '#eee' ,height:'58%' ,marginTop:64 , border:'none'}} swipeThreshold = {0.2} currentPage={1} initialPage={1}> 
+
+      <Image style={{width:100+'%',height:100+'%'}} source={require('./assets/3.jpg')}/>
+      <Image style={{width:100+'%',height:100+'%'}} source={require('./assets/2.jpg')}/>
+      <Image style={{width:100+'%',height:100+'%'}} source={require('./assets/1.jpg')}/>
+
+
+            
+        </Carousel> 
+
+       
+
+       <Text style={styles.text}>WELCOME TO ASORT</Text>
+
+      
+      <Text style={styles.cont}>Asort is a fashion e-commerce and lifestyle platform working 
+to change the way people shop fashion online</Text>
+
+      
+      <View style={styles.buttonView}>
+
+          <Button style={styles.signin}><Text style={{textAlign:'center' , flex:1}}>SIGN IN</Text></Button>  
+          <Button style={styles.signin}><Text style={{textAlign:'center', flex:1}}>SIGN UP</Text></Button>  
+
       </View>
+          
+       
+      </View>
+
+      </View>
+     
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:'center',
+    
   },
+  welcome:{
+    flex:0,
+    width:100+'%',
+    alignItems: 'center',
+    justifyContent:'center',
+  },
+text : {
+  top: 550,
+  position: "absolute",
+  backgroundColor: "transparent",
+ fontFamily:'Roboto',
+  fontSize: 20,
+  lineHeight: 140,
+  left:25+'%',
+  letterSpacing: 5
+},
+
+
+cont:{
+
+  top: 650,
+  width:80+'%',
+  position: "absolute",
+ left:10+'%',
+  backgroundColor: "transparent",
+  fontSize: 14,
+  textAlign:'center',
+  lineHeight:30,
+  color:'#ccc',
+
+},
+
+signin:{
+width:'50%',
+position: 'relative',
+textAlign:'center',
+backgroundColor:'#717171',
+height:60,
+borderRadius:0,
+borderRightWidth:0.5,
+borderColor:'#fff'
+},
+
+buttonView: {
+  flex: 1,
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  position:'absolute',
+  textAlign:'center',
+  bottom:0
+},
+ 
 });
---
+++ styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+let styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignSelf: "center",
+        height:400,
+    },
+    page: {
+        flex: 1
+    }
+});
+
+export default styles;
